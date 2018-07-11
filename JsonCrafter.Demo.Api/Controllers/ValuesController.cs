1bc5b2ac6895663e371fdeeaf0be48b55c817dbd
Daniel Ryhle <daniel@ryhle.se>
2018-07-11 22:17:27 +0200

    .

--- JsonCrafter.Demo.Api/Controllers/ValuesController.cs
+++ JsonCrafter.Demo.Api/Controllers/ValuesController.cs
@@ -8,18 +8,32 @@ namespace JsonCrafter.Demo.Api.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        public static Test TestObj = CreateTest();
+
         // GET api/values
         [HttpGet]
         public ActionResult<dynamic> Get()
         {
             //return new {Apa = "a"};
-            var t = new Test();
-            t.TestTests.Add(new Test());
-            return t;
+            //var t = new Test();
+            //t.TestTests.Add(new Test());
+            return TestObj;
             //return new string[] { "value1", "value2" };
         }
-    }
 
+        public static Test CreateTest()
+        {
+            var t = new Test();
+
+            for (int i = 0; i < 2000; i++)
+            {
+                t.TestTests.Add(new Test { Name = "ChildTest" + i });
+            }
+
+            return t;
+        }
+    }
+    
     public class Test
     {
         public int Id { get; set; } = 1;
