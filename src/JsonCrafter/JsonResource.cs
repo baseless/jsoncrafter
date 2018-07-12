2692313834b22967f9e08fba20f9598a28813db2
Daniel Ryhle <daniel@ryhle.se>
2018-07-12 22:28:59 +0200

    .

--- src/JsonCrafter/JsonResource.cs
+++ src/JsonCrafter/JsonResource.cs
@@ -9,14 +9,14 @@ namespace JsonCrafter
         Link
     }
 
-    public sealed class JsonResource
+    public class JsonResource
     {
         public JsonResourceType Type { get; set; }
         public string Key { get; set; }
         public string Url { get; set; }
     }
 
-    public class JsonResource<T> : JsonResource where T: class, new()
+    public sealed class JsonResource<T> : JsonResource where T: class, new()
     {
         public Expression<Func<T, string>>[] Properties { get; set; }
     }
