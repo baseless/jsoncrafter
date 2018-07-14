3f634b2c9772a1f7d9e239ab0c4a30184f2479e5
Daniel Ryhle <daniel@ryhle.se>
2018-07-15 01:39:49 +0200

    .

--- src/JsonCrafter.Conversion/Hal/HalTemplateBuilder.cs
+++ src/JsonCrafter.Conversion/Hal/HalTemplateBuilder.cs
@@ -14,12 +14,45 @@ namespace JsonCrafter.Conversion.Hal
 
         public IDictionary<Type, ITypeTemplate> BuildTypeTemplates()
         {
-            throw new NotImplementedException();
+            return new Dictionary<Type, ITypeTemplate>();
         }
         
         public ITypeBuilder<T> For<T>() where T : class
         {
-            throw new System.NotImplementedException();
+            return new TypeBuilder<T>();
+        }
+
+        public class TypeBuilder<T> : ITypeBuilder<T> where T : class
+        {
+            public ITypeBuilder<T> HasTemplate(string url, string templateIdentifier)
+            {
+                return this;
+            }
+
+            public ITypeBuilder<T> HasRelatedResource(Func<T, object> resource)
+            {
+                return this;
+            }
+
+            public ITypeBuilder<T> HasLinkToSelf(string url, params string[] values)
+            {
+                return this;
+            }
+
+            public ITypeBuilder<T> HasLinkToSelf(string url, params Func<T, object>[] values)
+            {
+                return this;
+            }
+
+            public ITypeBuilder<T> HasLink(string name, string templateId, string url, params Func<T, object>[] values)
+            {
+                return this;
+            }
+
+            public ITypeBuilder<T> HasLink(string name, string templateId, string url, params string[] values)
+            {
+                return this;
+            }
         }
     }
 }
