f5947acde8d1e884a077c0dfdf98ecf4731bc676
Daniel Ryhle <daniel@ryhle.se>
2018-07-17 18:51:41 +0200

    .

---
+++ src/JsonCrafter.Serialization/Converters/Hale/HaleSpecification.cs
@@ -0,0 +1,16 @@
+﻿namespace JsonCrafter.Serialization.Converters.Hale
+{
+    /// <summary>
+    /// Hale - Hypertext Application Language Extension
+    /// https://github.com/mdsol/hale#hale---hypertext-application-language-extension
+    /// EXTENSION TO HAL, ALL HAL RULES APPLY.
+    ///
+    /// As a proper extension, every HAL document is intended to be a proper Hale document and vice versa. Hale provides two primary extensions to HAL.
+    /// Specifically it extends the properties of HAL Link Objects and adds a new reserved document keyword called "_meta" that defines document Reference Objects.
+    /// </summary>
+    public static class HaleSpecification
+    {
+
+        // Not processed
+    }
+}
